7d3a30f2ca5259a02d43f318a942a6d84143db08
cykoravish <ravishbisht85@gmail.com>
2025-05-17 12:08:14 +0530

    update the site map

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,13 +1,17 @@
-import type { MetadataRoute } from "next"
+import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: "/admin/",
+      disallow: [
+        "/admin/",
+        "/_next/static/media/",
+        "/_next/static/chunks/",
+        "/_next/image/",
+      ],
     },
     sitemap: "https://novanectar.co.in/sitemap.xml",
-  }
+  };
 }
-
