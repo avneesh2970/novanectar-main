9603aa760e9d86c30cf8b7592a6b6db12ab72402
cykoravish <ravishbisht85@gmail.com>
2025-06-16 11:48:08 +0530

    meta data type error fix

--- src/app/services/digital-marketing/page.tsx
+++ src/app/services/digital-marketing/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import DigitalMarketingClientPage from "./DigitalMarketingPage";
 import { Metadata } from "next";
 
-export const metaData: Metadata = {
+export const metadata: Metadata = {
   title: "Digital Marketing Companies in Dehradun | Novanectar",
   description:
     "Smart IT Solution is one of the best digital marketing companies in dehradun. we create stunning websites that are easy to navigate and make sense.",
